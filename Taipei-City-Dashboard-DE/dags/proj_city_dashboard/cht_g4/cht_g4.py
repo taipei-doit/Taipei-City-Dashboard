bef623cd378adaf48acb9f60118f10c33247b0a5
webber.ys.lin <webber.ys.lin@foxconn.com>
2024-12-17 14:41:28 +0800

    修改: 強制轉換型別

--- Taipei-City-Dashboard-DE/dags/proj_city_dashboard/cht_g4/cht_g4.py
+++ Taipei-City-Dashboard-DE/dags/proj_city_dashboard/cht_g4/cht_g4.py
@@ -57,11 +57,13 @@ def _cht_g4(**kwargs):
         sql = """select ev_name, gid from public.nye_grid"""
         engine = create_engine(ready_data_db_uri)
         g4_grids = pd.read_sql(sql, engine)
-
+        raw_data_df['gid'] = raw_data_df['gid'].astype(str)
+        g4_grids['gid'] = g4_grids['gid'].astype(str)
         merged_df = raw_data_df.merge(g4_grids, on='gid', how='left', suffixes=('_a', '_b'))
         # 更新 ev_name，當 gid 相同時使用 df_b 的 ev_name
         merged_df['ev_name'] = merged_df['ev_name_b'].combine_first(merged_df['ev_name_a'])
         merged_df = merged_df.drop(columns=['ev_name_b','ev_name_b'])
+        merged_df['gid'] = merged_df['gid'].astype(int)
         logging.info(merged_df.head)
         # Add additional columns
         merged_df['time'] = res['time']
