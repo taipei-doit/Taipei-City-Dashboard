841cab6a3299de74dbcc2d4c5a73353c0f2fb480
Yuhan <hyuhan629@gmail.com>
2025-05-05 17:06:55 +0800

    fix: geo_server使用location.origin並移除未使用程式碼

--- Taipei-City-Dashboard-FE/src/store/mapStore.js
+++ Taipei-City-Dashboard-FE/src/store/mapStore.js
@@ -28,8 +28,6 @@ import {
 	MapObjectConfig,
 	CityMapView,
 	TaipeiBuilding,
-	// TpDistrict,
-	// TpVillage,
 	metroTaipeiTown,
 	metroTaipeiVillage,
 	metroTpDistrict,
@@ -170,41 +168,20 @@ export const useMapStore = defineStore("map", {
 					scheme: "tms",
 					tolerance: 0,
 					tiles: [
-						`https://citydashboard.taipei/geo_server/gwc/service/tms/1.0.0/taipei_vioc:metrotaipei_village@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
-						// `${location.origin}/geo_server/gwc/service/tms/1.0.0/taipei_vioc:metrotaipei_village@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
-
+						`${location.origin}/geo_server/gwc/service/tms/1.0.0/taipei_vioc:metrotaipei_village@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
 					],
 				})
 				.addLayer(metroTpVillage);
-			// .addSource(`tp_village`, {
-			// 	type: "vector",
-			// 	scheme: "tms",
-			// 	tolerance: 0,
-			// 	tiles: [
-			// 		`${location.origin}/geo_server/gwc/service/tms/1.0.0/taipei_vioc:tp_village@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
-			// 	],
-			// })
-			// .addLayer(TpVillage);
-			// Taipei District Boundaries
 			this.map
 				.addSource(`metrotaipei_town`, {
 					type: "vector",
 					scheme: "tms",
 					tolerance: 0,
 					tiles: [
-						`https://citydashboard.taipei/geo_server/gwc/service/tms/1.0.0/taipei_vioc:metrotaipei_town@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
+						`${location.origin}/geo_server/gwc/service/tms/1.0.0/taipei_vioc:metrotaipei_town@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
 					],
 				})
 				.addLayer(metroTpDistrict);
-			// .addSource(`tp_district`, {
-			// 	type: "vector",
-			// 	scheme: "tms",
-			// 	tolerance: 0,
-			// 	tiles: [
-			// 		`${location.origin}/geo_server/gwc/service/tms/1.0.0/taipei_vioc:tp_district@EPSG:900913@pbf/{z}/{x}/{y}.pbf`,
-			// 	],
-			// })
-			// .addLayer(TpDistrict);
 
 			this.addSymbolSources();
 		},
