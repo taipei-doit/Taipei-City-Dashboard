19610b0ce914dee3ce4d01d0e7def061818171e4
freetime007 <99649841+freetime007@users.noreply.github.com>
2025-07-29 10:45:04 +0800

    修改: 資料來源異動

--- Taipei-City-Dashboard-DE/dags/proj_city_dashboard/dependency_ratio_and_aging_index/dependency_ratio_and_aging_index.py
+++ Taipei-City-Dashboard-DE/dags/proj_city_dashboard/dependency_ratio_and_aging_index/dependency_ratio_and_aging_index.py
@@ -14,7 +14,9 @@ def _transfer(**kwargs):
         update_lasttime_in_data_to_dataset_info,
     )
     from utils.transform_time import convert_str_to_time_format
-
+    import requests
+    from io import StringIO
+    
     # Config
     ready_data_db_uri = kwargs.get("ready_data_db_uri")
     dag_infos = kwargs.get("dag_infos")
@@ -22,10 +24,14 @@ def _transfer(**kwargs):
     load_behavior = dag_infos.get("load_behavior")
     default_table = dag_infos.get("ready_data_default_table")
     history_table = dag_infos.get("ready_data_history_table")
-    RID= "6701c78f-c781-426c-98b7-182827d93384"
-    # Load
-    res = get_data_taipei_api(RID)
-    raw_data = pd.DataFrame(res)
+    # 連結棄用
+    # RID= "6701c78f-c781-426c-98b7-182827d93384"
+    # res = get_data_taipei_api(RID)
+    
+    URL = 'https://tsis.dbas.gov.taipei/statis/webMain.aspx?sys=220&ymf=5700&kind=21&type=0&funid=a05002601&cycle=4&outmode=12&compmode=0&outkind=1&deflst=2&nzo=1'
+    response = requests.get(URL, verify=False)
+    # 讀取 CSV
+    raw_data = pd.read_csv(StringIO(response.text))
     raw_data["data_time"] = raw_data["_importdate"].iloc[0]["date"]
     print(f"raw data =========== {raw_data.head()}")
     data = raw_data.copy()
@@ -33,7 +39,7 @@ def _transfer(**kwargs):
     
     data = data.rename(
         columns={
-            "年底別": "end_of_year",
+            "統計期": "end_of_year",
             "幼年人口數[人]": "young_population",
             "幼年人口占全市人口比率[％]": "young_population_percentage",
             "青壯年人口數[人]": "working_age_population",
@@ -46,7 +52,7 @@ def _transfer(**kwargs):
             "老化指數[％]": "aging_index",
         }
     )
-    data['end_of_year'] = data['end_of_year'].replace('年', '', regex=True)
+    data['end_of_year'] = data['end_of_year'].replace('年底', '', regex=True)
     data['end_of_year'] = data['end_of_year'].astype(int) + 1911
     engine = create_engine(ready_data_db_uri)
     save_dataframe_to_postgresql(
