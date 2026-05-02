6e2cf1d469aa2fb0b32eb4708909efd99af9f4ee
Jian-An Wang <facebook0983481181@gmail.com>
2026-05-02 20:31:55 +0800

    feat: add the API

--- Taipei-City-Dashboard-BE/app/routes/router.go
+++ Taipei-City-Dashboard-BE/app/routes/router.go
@@ -204,6 +204,7 @@ func configureAIRoutes() {
 	aiRoutes.Use(middleware.IsLoggedIn())
 	{
 		aiRoutes.POST("/chat/twai", controllers.ChatWithTWCC)
+		aiRoutes.POST("/extract-insight/news", controllers.GetComponemtByNews)
 	}
 }
 
