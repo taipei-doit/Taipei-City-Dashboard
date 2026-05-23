5ea656b537400d0e1f2ecd4c977862d11c0ca8a7
SchumiYang <schumiy.tw@gmail.com>
2026-05-24 05:07:58 +0800

    feat: add  component SunburstChart

--- Taipei-City-Dashboard-FE/src/dashboardComponent/DashboardComponent.vue
+++ Taipei-City-Dashboard-FE/src/dashboardComponent/DashboardComponent.vue
@@ -28,6 +28,7 @@ import BarChartWithGoal from "./components/BarChartWithGoal.vue";
 import IconPercentChart from "./components/IconPercentChart.vue";
 import IndicatorChart from "./components/IndicatorChart.vue";
 import TextUnitChart from "./components/TextUnitChart.vue";
+import SunburstChart from "./components/SunburstChart.vue";
 
 import MapLegendSvg from "./assets/chart/MapLegend.svg";
 import DistrictChartSvg from "./assets/chart/DistrictChart.svg";
@@ -48,7 +49,7 @@ import BarChartWithGoalSvg from "./assets/chart/BarChartWithGoal.svg";
 import TreemapChartSvg from "./assets/chart/TreemapChart.svg";
 import IndicatorChartSvg from "./assets/chart/IndicatorChart.svg";
 import TextUnitChartSvg from "./assets/chart/TextUnitChart.svg";
-
+import SunburstChartSvg from "./assets/chart/SunburstChart.svg";
 
 const props = defineProps({
 	style: { type: Object, default: () => ({}) },
@@ -222,6 +223,8 @@ function returnChartComponent(name, svg) {
 		return svg ? IndicatorChartSvg : IndicatorChart;
 	case "TextUnitChart":
 		return svg ? TextUnitChartSvg : TextUnitChart;
+	case "SunburstChart":
+		return svg ? SunburstChartSvg : SunburstChart;
 	default:
 		return svg ? MapLegendSvg : MapLegend;
 	}
