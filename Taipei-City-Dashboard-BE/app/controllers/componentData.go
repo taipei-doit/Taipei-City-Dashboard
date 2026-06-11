fcc4ca3e2bd5523fe89934d6b7aaf6cb1dde82f4
zylin-omniguider <zylin@omniguider.com>
2026-06-11 18:41:11 +0800

    Feature/20260610 01 adjust component chart api (#1310)
    **IMPORTANT: Please do not create a Pull Request without creating an
    issue first.**
    
    ## Summary
    
    **Issue:** Enter the issue(s) this pull request resolves.
    
    A clear and concise description of this pull request
    
    ## Type (Fill in "x" to check)
    
    - [ ] Bug Fix
    - [x] New Feature
    
    ## Checklist
    
    Please make sure that all items are checked before submitting this
    request.
    
    - [ ] Code linter has been run and issues have all been resolved
    - [ ] The code has been thoroughly tested and no visible bugs have been
    introduced
    - [ ] The pull request will completely resolve the issue(s) mentioned
    - [ ] The pull request only resolves the issue(s) mentioned and nothing
    more
    
    ## Additional context
    
    Add any other context here.
    
    (cherry picked from commit ccdc5a40993ebb1f0ab395979486eaebd1817487)

--- Taipei-City-Dashboard-BE/app/controllers/componentData.go
+++ Taipei-City-Dashboard-BE/app/controllers/componentData.go
@@ -84,12 +84,16 @@ func GetComponentChartData(c *gin.Context) {
 		}
 		c.JSON(http.StatusOK, gin.H{"status": "success", "data": chartData})
 	} else if queryType == "bubble" {
-		chartData, err := models.GetBubbleData(&queryString, timeFrom, timeTo)
+		chartData, categories, err := models.GetBubbleData(&queryString, timeFrom, timeTo)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": err.Error()})
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{"status": "success", "data": chartData})
+		if len(categories) > 0 {
+			c.JSON(http.StatusOK, gin.H{"status": "success", "data": chartData, "categories": categories})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"status": "success", "data": chartData})
+		}
 	} else if queryType == "layered_flow" {
 		chartData, categories, err := models.GetLayeredFlowData(&queryString, timeFrom, timeTo)
 		if err != nil {
