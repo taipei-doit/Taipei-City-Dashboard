eed68cd81bdf8cdebeda74b9d25656b5ce60e895
dean <dean.liu@omniguider.com>
2026-06-17 09:49:10 +0800

    新增: 因應黑客松整併新增風場功能

--- Taipei-City-Dashboard-FE/src/dashboardComponent/components/BubbleChart.vue
+++ Taipei-City-Dashboard-FE/src/dashboardComponent/components/BubbleChart.vue
@@ -31,11 +31,11 @@ const tooltip = ref({
 });
 
 const parsedUnit = (() => {
-  try {
-    return JSON.parse(props.chart_config.unit);
-  } catch {
-    return props.chart_config.unit;
-  }
+	try {
+		return JSON.parse(props.chart_config.unit);
+	} catch {
+		return props.chart_config.unit;
+	}
 })();
 
 function showTooltip({ name, point, categories }) {
@@ -203,51 +203,51 @@ function handleDataSelection(_e, _chartContext, config) {
 </script>
 
 <template>
-	<div
-		v-if="activeChart === 'BubbleChart'"
-		ref="chartRef"
-		class="bubbleChart"
-		@mousemove="onChartMouseMove"
-		@mouseleave="hideTooltip"
-	>
-		<VueApexCharts
-			type="bubble"
-			width="100%"
-			height="100%"
-			:options="chartOptions"
-			:series="props.series"
-			@data-point-selection="handleDataSelection"
-		/>
+  <div
+    v-if="activeChart === 'BubbleChart'"
+    ref="chartRef"
+    class="bubbleChart"
+    @mousemove="onChartMouseMove"
+    @mouseleave="hideTooltip"
+  >
+    <VueApexCharts
+      type="bubble"
+      width="100%"
+      height="100%"
+      :options="chartOptions"
+      :series="props.series"
+      @data-point-selection="handleDataSelection"
+    />
 
-		<Teleport to="body">
-			<div
-				v-if="tooltip.visible && tooltip.point"
-				class="chart-tooltip chart-tooltip-bubble"
-				:style="{
-					top: tooltip.y + 'px',
-					left: tooltip.x + 'px',
-					transform:
-						tooltip.placement === 'left'
-							? 'translateX(-100%)'
-							: 'none',
-				}"
-			>
-				<h6>{{ tooltip.name }}</h6>
-				<div>
-					{{ tooltip.categories[0] ?? "X" }}：{{ tooltip.point.x }}
-					{{ parsedUnit?.x ?? "" }}
-				</div>
-				<div>
-					{{ tooltip.categories[1] ?? "Y" }}：{{ tooltip.point.y }}
-					{{ parsedUnit?.y ?? "" }}
-				</div>
-				<div>
-					{{ tooltip.categories[2] ?? "Z" }}：{{ tooltip.point.z }}
-					{{ parsedUnit?.z ?? "" }}
-				</div>
-			</div>
-		</Teleport>
-	</div>
+    <Teleport to="body">
+      <div
+        v-if="tooltip.visible && tooltip.point"
+        class="chart-tooltip chart-tooltip-bubble"
+        :style="{
+          top: tooltip.y + 'px',
+          left: tooltip.x + 'px',
+          transform:
+            tooltip.placement === 'left'
+              ? 'translateX(-100%)'
+              : 'none',
+        }"
+      >
+        <h6>{{ tooltip.name }}</h6>
+        <div>
+          {{ tooltip.categories[0] ?? "X" }}：{{ tooltip.point.x }}
+          {{ parsedUnit?.x ?? "" }}
+        </div>
+        <div>
+          {{ tooltip.categories[1] ?? "Y" }}：{{ tooltip.point.y }}
+          {{ parsedUnit?.y ?? "" }}
+        </div>
+        <div>
+          {{ tooltip.categories[2] ?? "Z" }}：{{ tooltip.point.z }}
+          {{ parsedUnit?.z ?? "" }}
+        </div>
+      </div>
+    </Teleport>
+  </div>
 </template>
 
 <style lang="scss" scoped>
