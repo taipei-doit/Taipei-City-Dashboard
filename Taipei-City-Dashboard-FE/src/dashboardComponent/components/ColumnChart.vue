6398cb297c6b0c461cb53862b2c9fbd8e6217d03
dean <dean.liu@omniguider.com>
2026-07-21 11:11:33 +0800

    調整: 調整column chart高度

--- Taipei-City-Dashboard-FE/src/dashboardComponent/components/ColumnChart.vue
+++ Taipei-City-Dashboard-FE/src/dashboardComponent/components/ColumnChart.vue
@@ -220,7 +220,7 @@ function resetWidth() {
       :key="chartWidth"
       type="bar"
       :width="chartWidth"
-      height="250px"
+      height="260px"
       :options="chartOptions"
       :series="series"
       @data-point-selection="handleDataSelection"
