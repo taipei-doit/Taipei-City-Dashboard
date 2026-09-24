1ea0a1d734c64b02a389d3f419d4dc509d3e3bc2
freetime007 <99649841+freetime007@users.noreply.github.com>
2026-09-24 16:02:05 +0800

    fix(cdc_enterovirus): fall back to official CSV over HTTP on HTTPS timeout

--- Taipei-City-Dashboard-DE/dags/proj_city_dashboard/D080102/D080102.py
+++ Taipei-City-Dashboard-DE/dags/proj_city_dashboard/D080102/D080102.py
@@ -1,4 +1,8 @@
+import logging
+
 from airflow import DAG
+from requests.exceptions import ConnectTimeout
+
 from operators.common_pipeline import CommonDag
 
 
@@ -6,9 +10,14 @@ def _D080102(**kwargs):
     from proj_city_dashboard.D080101.cdc_visit_case_etl import cdc_visit_case_etl
     
     URL = "https://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
+    HTTP_FALLBACK_URL = "http://od.cdc.gov.tw/eic/NHI_EnteroviralInfection.csv"
     DIEASE_NAME = '腸病毒'
     
-    cdc_visit_case_etl(URL, DIEASE_NAME, **kwargs)
+    try:
+        cdc_visit_case_etl(URL, DIEASE_NAME, **kwargs)
+    except ConnectTimeout:
+        logging.warning("CDC HTTPS connection timed out; using the HTTP CSV fallback")
+        cdc_visit_case_etl(HTTP_FALLBACK_URL, DIEASE_NAME, **kwargs)
 
 
 
